47cb8f021d896d211d062eafa63e79b858b1564f
yetbarek temesgen <yetbarek1@gmail.com>
2025-12-12 21:12:21 -0500

    Use Cloudinary videos for production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
 export class AppComponent {
   title = 'Nature by Yetbarek';
 
+  // Array of scene names
   scenes: string[] = ['desert', 'ocean', 'volcano', 'wildlife'];
 
+  // Cloudinary video URLs
   videoURLs: Record<string, string> = {
     desert: 'https://res.cloudinary.com/dffqpwber/video/upload/v1765589682/desert_z1vkbr.mp4',
     ocean: 'https://res.cloudinary.com/dffqpwber/video/upload/v1765589683/ocean_ajxo0i.mp4',
@@ -10,6 +19,7 @@ export class AppComponent {
     wildlife: 'https://res.cloudinary.com/dffqpwber/video/upload/v1765589691/wildlife.mp4'
   };
 
+  // Toggle play/pause when video is clicked
   toggleVideo(scene: string) {
     const video = document.getElementById('video-' + scene) as HTMLVideoElement;
     if (video) {
